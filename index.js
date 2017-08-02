ba89a098b5064f2e71bf67b368b545b1a8579ad5
Josep Subils <josep@spellbook.io>
2017-08-02 15:45:13 +0200

    [update] identation to spaces

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 var eem = {
-	func : {},
-	on : function (ev, callback) {
-		if (ev === undefined || ev === null || ev === "") throw new Error('Need event');
-		else if (ev instanceof Array) {
-			for (var y = 0; y < ev.length; y++) {
-				if (typeof callback !== 'function') throw Error('Event need function');
-				else this.func[ev[y]] = callback;
-			}
-    } else if (typeof callback !== 'function') throw Error('Event need function');
-		else this.func[ev] = callback;
-	},
-	emit : function (ev, data) {
-		if (ev === undefined || ev === null || ev === "") throw new Error('No event selected.');
-		else if (ev instanceof Array) {
-			for (var i = 0; i < ev.length; i++) {
-				if (typeof this.func[ev[i]] === 'function') this.func[ev[i]](data);
-			}
-		} else if (typeof this.func[ev] === 'function') this.func[ev](data);
-	},
-	remove : function (ev) {
-		if (ev === undefined || ev === null || ev === "") throw new Error('No event selected.');
-		else if (this.func[ev] === undefined) throw Error('This event not exist');
-		else delete this.func[ev];
-	}
-};
+  func : {},
+  on : function (ev, callback) {
+    if (ev === undefined || ev === null || ev === "") throw new Error('Need event')
+    else if (ev instanceof Array) {
+      for (var y = 0; y < ev.length; y++) {
+        if (typeof callback !== 'function') throw Error('Event need function')
+        else this.func[ev[y]] = callback
+      }
+    } else if (typeof callback !== 'function') throw Error('Event need function')
+    else this.func[ev] = callback
+  },
+  emit : function (ev, data) {
+    if (ev === undefined || ev === null || ev === "") throw new Error('No event selected.')
+    else if (ev instanceof Array) {
+      for (var i = 0; i < ev.length; i++) {
+        if (typeof this.func[ev[i]] === 'function') this.func[ev[i]](data)
+      }
+    } else if (typeof this.func[ev] === 'function') this.func[ev](data)
+  },
+  remove : function (ev) {
+    if (ev === undefined || ev === null || ev === "") throw new Error('No event selected.')
+    else if (this.func[ev] === undefined) throw Error('This event not exist')
+    else delete this.func[ev]
+  }
+}
 
-if (typeof process === 'object') module.exports = eem;
+if (typeof process === 'object') module.exports = eem
