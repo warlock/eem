879c99ecb270ae1f69ff33efaeefb5be22a54bdf
Josep Subils <josep@spellbook.io>
2017-08-10 09:29:23 +0200

    [doc] add jsdoc documentation

--- src/main.js
+++ src/main.js
@@ -1,26 +1,43 @@
 const tck = require('tck')
 
 const eem = {
-  func : {},
+  event_stack : {},
+  
+  /**
+   * @param {String} ev : The name of event.
+   * @param {Function} callback : Function executed when event is triggered.
+   * @returns {undefined}
+   */
   on : (ev, callback) => {
     if (tck.isEmpty(ev)) throw new Error('Need event key!')
     else if (!tck.isFunction(callback)) throw new Error('Event needs a function!')
     else if (tck.isArray(ev)) {
       for (let y = 0; y < ev.length; y++) {
-        eem.func[ev[y]] = callback
+        this.event_stack[ev[y]] = callback
       }
-    } else eem.func[ev] = callback
+    } else this.event_stack[ev] = callback
   },
+
+  /**
+   * @param {String} ev : The name of event.
+   * @param {Any} data : Data for a event callback.
+   * @returns {undefined}
+   */
   emit: (ev, data) => {
     if (tck.isEmpty(ev)) throw new Error('No event selected.')
     else if (tck.isArray(ev)) {
       for (let i = 0; i < ev.length; i++) {
-        if (tck.isFunction(eem.func[ev[i]])) eem.func[ev[i]](data)
+        if (tck.isFunction(this.event_stack[ev[i]])) this.event_stack[ev[i]](data)
       }
-    } else if (tck.isFunction(eem.func[ev])) eem.func[ev](data)
+    } else if (tck.isFunction(this.event_stack[ev])) this.event_stack[ev](data)
   },
+
+  /**
+   * @param {String} ev : The name of event for destroy.
+   * @returns {undefined}
+   */
   remove: ev => {
-    if (!tck.isEmpty(eem.func[ev])) delete eem.func[ev]
+    if (!tck.isEmpty(this.event_stack[ev])) delete this.event_stack[ev]
   }
 }
 
