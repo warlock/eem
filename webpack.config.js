d75e8305fe74a1d7f72c667f59b3a6beb9cc8a98
Josep Subils <josep@spellbook.io>
2017-10-18 08:54:47 +0200

    [up] update babel presets and tck type checker

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,16 @@
 const path = require('path')
 
 module.exports = {
-  "entry": "./src/main.js",
-  "output": {
-    "path": path.resolve("./"),
-    "library": "eem",
-    "filename": "eem.js",
-    "libraryTarget": "umd"
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve("./"),
+    library: "eem",
+    filename: "eem.js",
+    libraryTarget: "umd"
   },
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      }
+    rules: [
+      { test: /\.js$/, loader: "babel-loader" }
     ]
   }
 }
